23c3bcd003702eb79e49ffa124dcd2f8d0642ab1
ngocson <ngocson13btt@gmail.com>
2022-01-09 21:07:54 +0700

    lab2

---
+++ Review_code/Lab2.rb
@@ -0,0 +1,17 @@
+a = " chao"
+puts a
+
+b,c = 1,2
+puts "Sum a and b: #{b + c}"
+welcome = a + " son"
+puts welcome
+puts welcome * 5
+d = 2022
+print "Toi se doan so tuoi cua ban\n"
+print "Vui long nhap nam sinh cua ban:"
+ns = gets
+puts ns.to_f
+#chuyen doi sang float
+e = d - ns.to_i
+#chuyen sang int de tinh
+puts "Ban #{e} tuoi nha"
--- Review_code/README.md
+++ Review_code/README.md
@@ -1,1 +1,1 @@
-# Review code
+# Toturial
